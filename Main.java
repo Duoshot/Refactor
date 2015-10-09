f2b569c7d554432ff832ec7e2f630ecff863e71b
Duoshot <obiwannabe1337@hotmail,.com>
2015-10-09 09:57:46 -0600

    v8
    Extract class

--- Main.java
+++ Main.java
@@ -2,49 +2,6 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void drawRectangle(char appearance, int width, int height)
-	{
-		System.out.println("Rectangle");
-		for(int i = 0; i < height; i++)
-		{ 
-			for(int j = 0; j < width; j++)
-			{
-				System.out.print(appearance);
-			}
-			System.out.println();
-		}
-		System.out.println();
-	}
-	
-	public static void drawRightTriangle(char appearance, int height)
-	{
-		System.out.println("Right triangle");
-		for(int i = 1; i <= height; i++)
-		{
-			for(int j = height; j > 0; j--)
-			{
-				if(i < j)
-					System.out.print(" ");
-				else
-					System.out.print(appearance);
-			}
-			System.out.println();
-		}
-	}
-	
-	public static void drawLeftTriangle(char appearance, int height)
-	{
-		for(int i = 0; i < height; i++)
-		{
-			for(int j = height; j > i; j--)
-			{
-				System.out.print(appearance);
-			}
-			System.out.println();
-		}
-		System.out.println("Left triangle");
-	}
-	
 	public static void printBanner()
 	{
 		System.out.println("Last Name: Law");
@@ -64,7 +21,7 @@ public class Main {
 		System.out.print("	Rectangles: ");
 		System.out.println(rectangle);
 		System.out.print("	Right Triangles: ");
-		System.out.print(rTriangle);
+		System.out.println(rTriangle);
 		System.out.print("	Left Triangles: ");
 		System.out.println(lTriangle);
 		System.out.println();
@@ -72,7 +29,7 @@ public class Main {
 	
 	public static void printInstructions()
 	{
-		System.out.println("To draw a rectangle, enter RE<appearance><width><height>, e.g. RE#45");
+		System.out.println("To draw a rectangle, enter RE<appearance><height><width>, e.g. RE#45");
 		System.out.println("To draw a right triangle, enter RT<appearance><height>, e.g. RT^3");
 		System.out.println("To draw a left triangle, enter LT<appearance><height>, e.g. LT@6");
 		System.out.println("To display how many shapes have been drawn, enter PR");
@@ -91,6 +48,8 @@ public class Main {
 		{
 			printInstructions();
 			
+			Draw draw = new Draw();
+			
 			Scanner scanner = new Scanner(System.in);
 			String line = scanner.nextLine();		
 	
@@ -102,33 +61,30 @@ public class Main {
 			}
 
 			String shape = ""; // initialize everything to the base case
-			char appearance = '\0';
-			int height = 0;
-			int width = 0;
 			
 			shape = line.substring(0,2); //parse the string from the file into the first 2 characters
 		
 			if(shape.equals("RE"))
 			{
-				appearance = line.charAt(2); // get the symbol used to draw the shape
-				height = Integer.parseInt(line.substring(3,4)); // get height
-				width = Integer.parseInt(line.substring(4)); // get width
-				drawRectangle(appearance, height, width);
+				draw.appearance = line.charAt(2); // get the symbol used to draw the shape
+				draw.height = Integer.parseInt(line.substring(3,4)); // get height
+				draw.width = Integer.parseInt(line.substring(4)); // get width
+				draw.drawRectangle();
 				numRectangle++;
 			}
 			else if(shape.equals("RT"))
 			{
-				appearance = line.charAt(2);  // get the symbol used to draw the shape
-				height = Integer.parseInt(line.substring(3)); //Get the size
-				drawRightTriangle(appearance, height);
+				draw.appearance = line.charAt(2);  // get the symbol used to draw the shape
+				draw.height = Integer.parseInt(line.substring(3)); //Get the size
+				draw.drawRightTriangle();
 				numRightTriangle++;
 			}
 		
 			else if(shape.equals("LT"))
 			{
-				appearance = line.charAt(2);  // get the symbol used to draw the shape
-				height = Integer.parseInt(line.substring(3)); //Get the size
-				drawLeftTriangle(appearance, height);
+				draw.appearance = line.charAt(2);  // get the symbol used to draw the shape
+				draw.height = Integer.parseInt(line.substring(3)); //Get the size
+				draw.drawLeftTriangle();
 				numLeftTriangle++;
 			}
 			else if(shape.equals("PR"))
